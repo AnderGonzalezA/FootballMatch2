15c94139d803f4d231f5781e9cd98c16bb45689d
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2018-11-23 10:51:36 +0100

    23/11

---
+++ src/Player.java
@@ -0,0 +1,21 @@
+public class Player {
+	
+	private String playerName;
+	private String teamName;
+	
+	public String getPlayerName() {
+		return this.playerName;
+	}
+	
+	public void setPlayerName(String PlayerName) {
+		this.playerName=PlayerName;
+	}
+
+	public String getTeamName() {
+		return this.teamName;
+	}
+	
+	public void setTeamrName(String TeamName) {
+		this.teamName=TeamName;
+	}
+}
